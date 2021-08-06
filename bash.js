8dd788d41149a2572c65bfe7fbbe2496d42146fe
anwsimon <anna.w.simon1@gmail.com>
2021-08-06 15:22:16 -0400

    updated pwd and ls

--- bash.js
+++ bash.js
@@ -1,14 +1,11 @@
-process.stdout.write('prompt > ');
-
 process.stdin.on('data', (data)=> {
     const cmd = data.toString().trim();
 
     process.stdout.write('You type: ' + cmd);
-    process.stdout.write('\nprompt > ');
-
-    if(cmd === 'pwd'){
-        //process.stdout.write('/Users/gabriellaapeadu/node-shell');
-        console.log(process.cwd());
-    }
 });
 
+const pwd = require('./pwd');
+pwd();
+
+const ls = require('./ls')
+ls();
---
+++ pwd.js
@@ -0,0 +1,7 @@
+
+
+module.exports = function(){
+
+    console.log(process.cwd());
+    process.stdout.write('\nprompt > ');
+}
